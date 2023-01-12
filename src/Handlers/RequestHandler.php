13fe8f17d086a391dc95f6e4de71ea16706e0fc6
AliReaza <alireazamagh@gmail.com>
2023-01-12 20:36:41 +0330

    Check if the request message is JSON or not

--- src/Handlers/RequestHandler.php
+++ src/Handlers/RequestHandler.php
@@ -20,8 +20,25 @@ class RequestHandler implements HandlerInterface
 
     public function __invoke(MessageInterface $message): void
     {
+        $content = $message->getContent();
+
+        $array = json_decode($content, true);
+
+        if (json_last_error() === JSON_ERROR_NONE) {
+            $array = [
+                'status' => 'Valid',
+                'body' => $array['request'] ?? null,
+                'files' => $array['files'] ?? null,
+            ];
+        } else {
+            $array = [
+                'status' => 'Invalid',
+                'body' => $content,
+            ];
+        }
+
         $message = new Message(
-            content: 'Request received.',
+            content: json_encode($array),
             causation_id: $message->getCausationId(),
             correlation_id: $message->getCorrelationId(),
             name: $this->response_message_name,
